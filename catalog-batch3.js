4d6a0e04f38a6920ed547e8f0216bf59929af7cb
Nicolasruiza <141341194+Nicolasruiza@users.noreply.github.com>
2026-09-11 17:36:11 -0400

    Add wardrobe images for new henley variants

---
+++ catalog-batch3.js
@@ -0,0 +1,6 @@
+window.FITS_CATALOG_IMAGES=window.FITS_CATALOG_IMAGES||{};
+Object.assign(window.FITS_CATALOG_IMAGES,{
+'navy-henley':'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_213523_e3dfc4ae-b9b4-4b34-8220-b18dfda5779e.png',
+'grey-henley':'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_213523_7e89498f-3666-4d47-9c1e-572b654993ca.png',
+'taupe-suede-shoes':'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_213524_c10d25dd-b11d-4274-9951-c4f32ba40b6c.png'
+});
