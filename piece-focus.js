461587f66f04deb619d49c09af006915a71f3757
Nicolasruiza <141341194+Nicolasruiza@users.noreply.github.com>
2026-09-11 17:50:08 -0400

    Use approved outfit crops for wardrobe item photos

---
+++ piece-focus.js
@@ -0,0 +1,36 @@
+(()=>{
+  const F={
+    'green-polo':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_113208_3fedb759-ebd0-4afb-b34c-06d1978bb3ab_min.webp',x:50,y:34,s:2.05},
+    'khaki-chinos':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_113208_3fedb759-ebd0-4afb-b34c-06d1978bb3ab_min.webp',x:50,y:62,s:2.0},
+    'navy-bomber':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212239_89e45f0a-7668-462b-9388-587f3ed67837.png',x:50,y:35,s:2.0},
+    'grey-tee':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212239_89e45f0a-7668-462b-9388-587f3ed67837.png',x:50,y:34,s:2.25},
+    'tan-chelsea':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212239_89e45f0a-7668-462b-9388-587f3ed67837.png',x:50,y:91,s:2.35},
+    'charcoal-cardigan':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212356_f8a6d9b4-46e5-4b43-96d9-b6852f9693bb.png',x:50,y:36,s:2.0},
+    'white-tee':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212356_f8a6d9b4-46e5-4b43-96d9-b6852f9693bb.png',x:50,y:34,s:2.25},
+    'dark-chinos':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212356_f8a6d9b4-46e5-4b43-96d9-b6852f9693bb.png',x:50,y:63,s:2.0},
+    'black-chelsea':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212356_f8a6d9b4-46e5-4b43-96d9-b6852f9693bb.png',x:50,y:91,s:2.35},
+    'charcoal-overcoat':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212357_05d75c03-518b-451b-b8d0-528723b8d63d.png',x:50,y:38,s:1.8},
+    'navy-crewneck':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212357_05d75c03-518b-451b-b8d0-528723b8d63d.png',x:50,y:35,s:2.25},
+    'brown-leather-jacket':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212356_be529c6a-c78a-4dc2-a6b4-9711a072a72f.png',x:50,y:36,s:2.0},
+    'black-turtleneck':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212356_be529c6a-c78a-4dc2-a6b4-9711a072a72f.png',x:50,y:33,s:2.3},
+    'beige-chinos':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212356_be529c6a-c78a-4dc2-a6b4-9711a072a72f.png',x:50,y:64,s:2.0},
+    'brown-chelsea':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212356_be529c6a-c78a-4dc2-a6b4-9711a072a72f.png',x:50,y:91,s:2.35},
+    'light-blue-oxford':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212356_25ef2519-9bbe-4ba0-8137-d94e3a7dd715.png',x:50,y:34,s:2.2},
+    'tan-suede-derbies':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212356_25ef2519-9bbe-4ba0-8137-d94e3a7dd715.png',x:50,y:91,s:2.35},
+    'beige-blazer':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212456_1fa961de-ca06-42e7-b967-5d7fcaaf436f.png',x:50,y:36,s:2.0},
+    'white-shirt':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212456_1fa961de-ca06-42e7-b967-5d7fcaaf436f.png',x:50,y:34,s:2.25},
+    'navy-cardigan':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212456_c0f39832-be2b-48ac-a08a-e926c3a94a6c.png',x:50,y:36,s:2.0},
+    'light-grey-trousers':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212456_c0f39832-be2b-48ac-a08a-e926c3a94a6c.png',x:50,y:64,s:2.0},
+    'taupe-suede-shoes':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212456_c0f39832-be2b-48ac-a08a-e926c3a94a6c.png',x:50,y:91,s:2.35},
+    'navy-henley':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212456_a036838a-1271-44a8-b7aa-31ea3791441c.png',x:50,y:34,s:2.2},
+    'grey-henley':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212539_85945672-4424-4158-b528-f8e5489bb551.png',x:50,y:34,s:2.2},
+    'olive-henley':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212540_780309e8-c244-4baa-b995-d917e2791690.png',x:50,y:34,s:2.2},
+    'blue-denim':{src:'https://d8j0ntlcm91z4.cloudfront.net/user_3CUt7UpZE1V87ctJC99CMT6AYSe/hf_20260911_212540_780309e8-c244-4baa-b995-d917e2791690.png',x:50,y:64,s:2.0}
+  };
+  const base=piecePhoto;
+  piecePhoto=function(p){
+    const f=F[p.id];
+    if(!f)return base(p);
+    return `<img class="piece-focus-img" src="${f.src}" alt="${p.name}" loading="lazy" style="--pf-x:${f.x}%;--pf-y:${f.y}%;--pf-s:${f.s}">`;
+  };
+})();
