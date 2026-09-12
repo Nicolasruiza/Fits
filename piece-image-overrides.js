a8ef608c2a25ca42240c720bc3077938f0a2ca07
Nicolasruiza <141341194+Nicolasruiza@users.noreply.github.com>
2026-09-12 07:57:42 -0400

    Fix missing wardrobe piece images

--- piece-image-overrides.js
+++ piece-image-overrides.js
@@ -1,1 +1,4 @@
-Object.assign(PIECE_IMAGES,window.FITS_CATALOG_IMAGES||{});
+Object.assign(PIECE_IMAGES,window.FITS_CATALOG_IMAGES||{}, {
+  'charcoal-trousers':'assets/piece-charcoal-chinos.webp?v=14',
+  'navy-quarterzip':'assets/piece-navy-quarterzip.webp?v=14'
+});
