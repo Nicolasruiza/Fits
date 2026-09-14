53c2acee3b6e6f7c46668c6274e95933c637c375
Nicolasruiza <141341194+Nicolasruiza@users.noreply.github.com>
2026-09-13 20:49:24 -0400

    Add cream chinos wardrobe image

--- piece-image-overrides.js
+++ piece-image-overrides.js
@@ -1,4 +1,5 @@
 Object.assign(PIECE_IMAGES,window.FITS_CATALOG_IMAGES||{}, {
   'charcoal-trousers':'assets/piece-charcoal-chinos.webp?v=14',
-  'navy-quarterzip':'assets/piece-navy-quarterzip.webp?v=14'
+  'navy-quarterzip':'assets/piece-navy-quarterzip.webp?v=14',
+  'cream-chinos':'assets/piece-cream-chinos.webp?v=1'
 });
