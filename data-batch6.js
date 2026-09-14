2fd306b7440a49cc27479356f32056cb69759c39
Nicolasruiza <141341194+Nicolasruiza@users.noreply.github.com>
2026-09-14 19:21:39 -0400

    Use physical WebP for look 38

--- data-batch6.js
+++ data-batch6.js
@@ -3,5 +3,5 @@ const addFamily=f=>{if(!D.families.some(x=>x.id===f.id))D.families.push(f)};
 const addLook=l=>{if(!D.looks.some(x=>x.id===l.id))D.looks.push(l)};
 // Approved final only. Inspiration/reference cards stay out of Looks.
 addFamily({id:'family-navy-crewneck-beige',name:'Navy crewneck + beige chinos',hero:'look-38',looks:['look-38']});
-addLook({id:'look-38',family:'family-navy-crewneck-beige',name:'Navy crewneck + light blue Oxford + beige chinos',image:'',formal:3,badges:['Office','Weekend'],pieces:['navy-crewneck','light-blue-oxford','beige-chinos','white-leather-sneakers']});
+addLook({id:'look-38',family:'family-navy-crewneck-beige',name:'Navy crewneck + light blue Oxford + beige chinos',image:'assets/look-38-navy-crewneck-beige.webp?v=1',formal:3,badges:['Office','Weekend'],pieces:['navy-crewneck','light-blue-oxford','beige-chinos','white-leather-sneakers']});
 })();
